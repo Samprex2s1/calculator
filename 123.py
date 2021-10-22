a073a3878e2c0419ebcbe37305105a6947865941
Samprex2s1 <92262036+Samprex2s1@users.noreply.github.com>
2021-10-22 16:32:11 +0600

    Add files via upload

--- 123.py
+++ 123.py
@@ -1,13 +1,20 @@
+#импортирование колорама для изменения цвета букв и заднего фона
 from colorama import init
 # use Colorama to make Termcolor work on Windows too
 init()
 from colorama import Fore, Back, Style
+#задний фон и цвет букв
 print(Fore.BLACK)
 print (Back.CYAN)
+#ввод первого числа
 a=float (input ('введи первое число: '))
+#ввод второго числа
 b=float (input ('введи второе число: '))
+#задний фон результата зеленый
 print (Back.GREEN)
-what=input ("что ты хочешь выполнить +:")
+#переменная которая спрашивает какую операцию провести
+what=input ("что ты хочешь выполнить +;/;*;-:")
+#различные операции в зависимости от значения if
 if what=='+':
 	c=a+b
 	print ('резултат'+ str(c))
@@ -20,6 +27,7 @@ elif what =="/":
 elif what =="*":
 	c=a*b
 	print ('результат '+ str(c))
+#вывод при неверно выбраном what
 else:
 	print ('неверная операция')
 
